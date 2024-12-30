3cdc09c32c05a8997c838d9de0249f00683aaef2
hold102 <113155551+hold102@users.noreply.github.com>
2024-12-30 22:08:40 +0800

    Create Main.java

---
+++ src/Viva3Q2/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args ){
+
+        Zoo myZoo = new Zoo(3);
+
+        myZoo.addCreature("Panda" , 150.0 , "Mountains");
+        myZoo.addCreature("Dragon" , 300.0 , "Cave");
+        myZoo.addCreature("Master Oogway" , 200.0 , "Forest");
+
+        myZoo.addCreature("Patrick Star", 250.0 , "Rock");
+
+        myZoo.displayAllCreatures();
+        myZoo.feedCreature("Dragon" , 50.0);
+        myZoo.displayAllCreatures();
+    }
+}
